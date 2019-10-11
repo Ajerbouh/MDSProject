35e4f1a9365341ab50ba0444dcfcc4e15efd4397
Ajerbouh <a.jerbouh@ecole-ipssi.com>
2019-10-11 14:34:43 +0200

    Car seeds added

---
+++ database/seeds/CarSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Faker\Factory as Faker;
+use Faker\Provider\Fakecar;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CarSeeder extends Seeder
+{
+
+    private function databaseInsert()
+    {
+        $faker = Faker::create();
+        $faker->addProvider(new Fakecar($faker));
+
+        DB::table('cars')->insert([
+            'name' => $faker->vehicleBrand,
+            'price' => $faker->randomNumber(4),
+        ]);
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        foreach (range(1, 10) as $index) {
+            $this->databaseInsert();
+        }
+    }
+}
